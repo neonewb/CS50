0079203c313e382e670085ba4d73b5775544be49
neonewb <imperror555@gmail.com>
2021-01-02 19:11:03 +0700

    init

---
+++ problemSet1/mario.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "cs50.h"
+
+int main (void)
+{
+  int height;
+  do
+  {
+    height = get_int("Height: ");
+  }
+  while (height < 1 || height > 8);
+  
+  for (int row = 0; row < height; row++)
+  {
+    for (int dot = 0; dot < height - (row + 1); dot++)
+    {
+      printf(".");
+    }
+    for (int hash = 0; hash < row + 1; hash++)
+    {
+      printf("#");
+    }
+    printf("\n");
+  }
+}
